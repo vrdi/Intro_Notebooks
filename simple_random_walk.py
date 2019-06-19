4659458cafa0884ac28862b2a84f16aaa6902667
Daryl DeFord <daryl.r.deford@gmail.com>
2019-06-19 11:59:40 -0400

    Create simple_random_walk.py

---
+++ simple_random_walk.py
@@ -0,0 +1,22 @@
+g= nx.grid_graph([5,5])
+state = random.choice(list(g.nodes()))
+states = []
+node_colors = {x:0 for x in g.nodes}
+edge_colors = {x:0 for x in g.edges}
+node_colors[state]=1
+nx.draw(g,pos = {x:x for x in g.nodes()}, node_color = [node_colors[x] for x in g.nodes()],edge_color = [edge_colors[x] for x in g.edges()],
+        width =3, cmap="jet")
+for i in range(5):
+    node_colors = {x:0 for x in g.nodes}
+    edge_colors = {x:0 for x in g.edges}
+    old_state = state
+    states.append(state)   
+    state = random.choice(list(g.neighbors(state)))
+    edge_colors[(old_state, state)]= 1
+    edge_colors[(state, old_state)]= 1
+    node_colors[old_state] = 2
+    node_colors[state]=1
+    plt.figure()
+    nx.draw(g,pos = {x:x for x in g.nodes()}, node_color = [node_colors[x] for x in g.nodes()],edge_color = [edge_colors[x] for x in g.edges()],
+        width =3, cmap="jet")
+    plt.show()
